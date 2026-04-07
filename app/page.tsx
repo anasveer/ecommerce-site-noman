8dba6eccf391959247813e224c915f8b31febe7c
Anas <anasaslam461@gmail.com>
2026-04-07 16:22:05 +0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,49 @@
 import React from 'react'
-import Hero from "../components/hero";
-import CategoryShowcase from "../components/CategoryShowcase";
-// import ProductSection from "../components/product1";
-import Showcase3 from "../components/showcase3";
-import S1 from "../components/showcase1";
-import Fabric from "../components/ComforterSet";
-import Showcase2 from "../components/showcase2";
-import Singal from "../components/SinglePair";
-import ThreePcsBedsheet from "../components/ThreePcsBedsheet";
-import DigitalArtistryShowcase from "../components/pixel";
-import Room from "../components/room";
-import Look from "../components/look";
-import Quality from "../components/quality";
-import Testimonials from "../components/Testimonials";
+// import Hero from "../components/hero";
+// import CategoryShowcase from "../components/CategoryShowcase";
+// // import ProductSection from "../components/product1";
+// import Showcase3 from "../components/showcase3";
+// import S1 from "../components/showcase1";
+// import Fabric from "../components/ComforterSet";
+// import Showcase2 from "../components/showcase2";
+// import Singal from "../components/SinglePair";
+// import ThreePcsBedsheet from "../components/ThreePcsBedsheet";
+// import DigitalArtistryShowcase from "../components/pixel";
+// import Room from "../components/room";
+// import Look from "../components/look";
+// import Quality from "../components/quality";
+// import Testimonials from "../components/Testimonials";
 
-const page = () => {
-  return (
-    <div>
-      <Hero />
-      <CategoryShowcase />
-      <ThreePcsBedsheet />
-      {/* <ProductSection/> */}
-      <Showcase3 />
-      <S1 />
-      <Fabric />
-      <Showcase2 />
-      <Singal />
-      <DigitalArtistryShowcase />
-      <Room />
-      <Look />
-      <Testimonials />
-      <Quality />
-    </div>
-  );
+ export default function page() {
+  console.log("MONGO_URI:", process.env.MONGODB_URI);
+  console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
+  return <h1>Homepage Working</h1>;
 }
 
-export default page
+
+
+// const page = () => {
+//   return (
+//     <div>
+//       <Hero />
+//       <CategoryShowcase />
+//       <ThreePcsBedsheet />
+//       {/* <ProductSection/> */}
+//       <Showcase3 />
+//       <S1 />
+//       <Fabric />
+//       <Showcase2 />
+//       <Singal />
+//       <DigitalArtistryShowcase />
+//       <Room />
+//       <Look />
+//       <Testimonials />
+//       <Quality />
+       
+  
+//     </div>
+//   );
+// }
+
+// export default page
