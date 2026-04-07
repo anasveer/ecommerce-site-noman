28be59886e8c728c631d4e8a44068aeb2fecd14a
Anas <anasaslam461@gmail.com>
2026-04-07 16:28:08 +0500

    add test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-// import Hero from "../components/hero";
-// import CategoryShowcase from "../components/CategoryShowcase";
-// // import ProductSection from "../components/product1";
+ import Hero from "../components/hero";
+import CategoryShowcase from "../components/CategoryShowcase";
 // import Showcase3 from "../components/showcase3";
 // import S1 from "../components/showcase1";
 // import Fabric from "../components/ComforterSet";
@@ -14,36 +13,31 @@ import React from 'react'
 // import Quality from "../components/quality";
 // import Testimonials from "../components/Testimonials";
 
- export default function page() {
-  console.log("MONGO_URI:", process.env.MONGODB_URI);
-  console.log("JWT_SECRET:", process.env.JWT_SECRET);
-
-  return <h1>Homepage Working</h1>;
-}
+ 
 
 
 
-// const page = () => {
-//   return (
-//     <div>
-//       <Hero />
-//       <CategoryShowcase />
-//       <ThreePcsBedsheet />
-//       {/* <ProductSection/> */}
-//       <Showcase3 />
-//       <S1 />
-//       <Fabric />
-//       <Showcase2 />
-//       <Singal />
-//       <DigitalArtistryShowcase />
-//       <Room />
-//       <Look />
-//       <Testimonials />
-//       <Quality />
+const page = () => {
+  return (
+    <div>
+       <Hero />
+      <CategoryShowcase />
+      {/* <ThreePcsBedsheet />
+      {/* <ProductSection/> */}
+      {/* <Showcase3 />
+      <S1 />
+      <Fabric />
+      <Showcase2 />
+      <Singal />
+      <DigitalArtistryShowcase />
+      <Room />
+      <Look />
+      <Testimonials />
+      <Quality /> */} 
        
   
-//     </div>
-//   );
-// }
+    </div>
+  );
+}
 
-// export default page
+export default page
